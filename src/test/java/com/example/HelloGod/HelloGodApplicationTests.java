bb111edca15c0887f1e2950f42a0f8a9492e703e
Juste Memphis <juste@menelic.com>
2021-04-15 11:56:15 +0100

    Hello

---
+++ src/test/java/com/example/HelloGod/HelloGodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloGod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
