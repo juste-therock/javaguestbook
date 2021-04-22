1119246377a506deb8b306198b0daf227fb2c87d
Juste Memphis <juste@menelic.com>
2021-04-22 16:43:28 +0100

    First step almost done

--- src/main/java/com/memphis/guestbook/Message.java
+++ src/main/java/com/memphis/guestbook/Message.java
@@ -1,6 +1,9 @@
 package com.memphis.guestbook;
 import javax.persistence.*;
 import javax.validation.constraints.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name = "guest_message")
@@ -23,6 +26,9 @@ public class Message {
     @Email
     private String email;
 
+    @Column(name="subject")
+    private String subject;
+
     @Column(name="age")
     @Min(value=18, message="Age should be greater than 17")
     private Integer age;
@@ -31,28 +37,59 @@ public class Message {
     @Column(name="content")
     private String content;
 
+    @Column(name="gender")
+    private String gender;
+
+    @Column(name="rating")
+    private Integer rating;
+
+    //Mapping TimeStamp using java.util.Date
+    @Column(name = "sentTime", updatable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date sentTime;
+    //private final SimpleDateFormat timeformat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+    //private java.util.Date parseTimestamp(String timestamp) {
+     //   try {
+     //       return timeformat.parse(timestamp);
+      //  } catch (ParseException e) {
+      //      throw new IllegalArgumentException(e);
+     //   }
+  //  }
+
+    //Mapping TimeStamp using LocalDateTime
+    // @Column(name = "sentTime")
+    //private LocalDateTime sentTime;
 
     public Integer getId() {return id; }
     public void setId(Integer id) { this.id = id; }
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
     public String getLastName() { return lastName; }
     public void setLastName(String lastName) {this.lastName = lastName; }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getSubject() { return subject; }
+    public void setSubject(String subject) { this.subject = subject; }
+
     public Integer getAge() {return age;}
     public void setAge(Integer age) {this.age = age;}
+
     public String getContent() { return content; }
     public void setContent(String content) {this.content = content; }
 
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+
+    public Integer getRating() { return rating;}
+    public void setRating() {this.rating = rating;}
+
+    public Date getSentTime() { return sentTime; }
+    public void setRating(Integer rating) { this.rating = rating; }
 
     @Override
     public String toString() {
@@ -61,8 +98,12 @@ public class Message {
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
+                ", email='" + subject + '\'' +
                 ", age=" + age +
                 ", content='" + content + '\'' +
+                ", gender='" + gender + '\'' +
+                ", rating=" + rating +
+                ", sentTime='" + sentTime + '\'' +
                 '}';
     }
 }
